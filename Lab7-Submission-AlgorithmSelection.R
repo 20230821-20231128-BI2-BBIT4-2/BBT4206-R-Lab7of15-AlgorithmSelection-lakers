cb619527309b194b0e1315b8197db9fa0d87e305
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-10-29 13:50:28 +0300

    split dataset

--- Lab7-Submission-AlgorithmSelection.R
+++ Lab7-Submission-AlgorithmSelection.R
@@ -5,3 +5,13 @@
 
 library(ggplot2)
 data("diamonds")
+
+library(caret)
+
+# Define an 80:20 train:test data split of the diamonds dataset
+set.seed(123)  # For reproducibility
+train_index <- createDataPartition(diamonds$price,
+                                   p = 0.8,
+                                   list = FALSE)
+diamonds_train <- diamonds[train_index, ]
+diamonds_test <- diamonds[-train_index, ]
