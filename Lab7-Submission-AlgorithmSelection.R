401268b4a16528e28750e01770c4cf7c96c3b26b
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-10-29 15:00:12 +0300

    split dataset

--- Lab7-Submission-AlgorithmSelection.R
+++ Lab7-Submission-AlgorithmSelection.R
@@ -175,4 +175,10 @@ print(paste("MAE =", sprintf(mae, fmt = "%#.4f")))
 library(mlbench)
 data("Sonar")
 
+# Define a 70:30 train:test data split of the dataset
+set.seed(123)  # For reproducibility
+train_index <- createDataPartition(Sonar$Class, p = 0.7, list = FALSE)
+sonar_train <- Sonar[train_index, ]
+sonar_test <- Sonar[-train_index, ]
+
 
