3c93bfbd9977fd5de32e55e2b2bd32c2e2c160f1
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-10-29 13:53:29 +0300

    Display the model's evaluation metrics

--- Lab7-Submission-AlgorithmSelection.R
+++ Lab7-Submission-AlgorithmSelection.R
@@ -24,3 +24,20 @@ summary(diamonds_model_lm)
 
 # Make predictions on the test set
 predictions <- predict(diamonds_model_lm, newdata = diamonds_test)
+
+# SSR
+ssr <- sum((diamonds_test$price - predictions)^2)
+print(paste("SSR =", sprintf(ssr, fmt = "%#.4f")))
+
+# SST
+sst <- sum((diamonds_test$price - mean(diamonds_test$price))^2)
+print(paste("SST =", sprintf(sst, fmt = "%#.4f")))
+
+# R Squared
+r_squared <- 1 - (ssr / sst)
+print(paste("R Squared =", sprintf(r_squared, fmt = "%#.4f")))
+
+# MAE
+absolute_errors <- abs(predictions - diamonds_test$price)
+mae <- mean(absolute_errors)
+print(paste("MAE =", sprintf(mae, fmt = "%#.4f")))
